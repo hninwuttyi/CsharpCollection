10d4c9c4735a641a2dd904b0ab7ad28e06f7a3ac
Hnin Wuttyi <wuttyi@solidplm.com>
2020-06-28 22:30:42 +0630

    Array simple.

---
+++ CsharpCollection/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string monday = "Monday";
+
+            string[] daysOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            Console.WriteLine("Which day do you want to display?");
+            Console.Write("(Monday=1, etc. ) >");
+            int iday = int.Parse(Console.ReadLine());
+            string chosenDay = daysOfWeek[iday];
+            Console.WriteLine($"That day is {chosenDay}");
+        }
+    }
+}
