1f800de21ae3b605812926261f5e96050b1e21ae
Hnin Wuttyi <wuttyi@solidplm.com>
2020-07-03 22:36:34 +0630

    Preparing for TopTenPops project

---
+++ TopTenPos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopTenPos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\SolidCad\Learning\PluralSight_Courses\Beginning C# Collections\csharp-collections-beginning\Pop by Largest Final.csv";
+        }
+    }
+}
