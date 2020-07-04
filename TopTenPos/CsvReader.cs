f317bcdb452b7d3143f4ac59e72e70762bf7e18c
Hnin Wuttyi <wuttyi@solidplm.com>
2020-07-04 21:46:05 +0630

    18.Instantiating an Array by Size

--- TopTenPos/CsvReader.cs
+++ TopTenPos/CsvReader.cs
@@ -8,5 +8,16 @@ namespace TopTenPos
 {
     class CsvReader
     {
+        private string _csvFilePath;
+        public CsvReader(string csvFilePath)
+        {
+            this._csvFilePath = csvFilePath;
+                 
+        }
+        public Country[] ReadFirstNCountries(int nCountries)
+        {
+            Country[] countries = new Country[nCountries];
+            return countries;
+        }
     }
 }
--- TopTenPos/Program.cs
+++ TopTenPos/Program.cs
@@ -11,6 +11,15 @@ namespace TopTenPos
         static void Main(string[] args)
         {
             string filePath = @"C:\SolidCad\Learning\PluralSight_Courses\Beginning C# Collections\csharp-collections-beginning\Pop by Largest Final.csv";
+            CsvReader reader = new CsvReader(filePath);
+
+            Country[] Countries = reader.ReadFirstNCountries(10);
+            
+            foreach(Country country in Countries)
+            {
+                Console.WriteLine($"{country.Population}: {country.Name}");
+            }
+
         }
     }
 }
