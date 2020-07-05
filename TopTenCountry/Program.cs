4e3e1f0d8b7a12c7795d7f40b8faf8b407a9e96f
Hnin Wuttyi <wuttyi@solidplm.com>
2020-07-05 22:12:22 +0630

    22.Populating the Array with a For Loop

--- TopTenCountry/Program.cs
+++ TopTenCountry/Program.cs
@@ -10,6 +10,16 @@ namespace TopTenCountry
     {
         static void Main(string[] args)
         {
+            string filePath = @"C:\SolidCad\Learning\PluralSight_Courses\Beginning C# Collections\csharp-collections-beginning\Pop by Largest Final.csv";
+            CsvReader reader = new CsvReader(filePath);
+
+            Country[] Countries = reader.ReadFirstNCountries(10);
+
+            foreach (Country country in Countries)
+            {
+                Console.WriteLine($"{country.Population}: {country.Name}");
+            }
+
         }
     }
 }
