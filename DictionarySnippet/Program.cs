574d804a265a50d63791e6b9f54aabaae8aafdd9
Hnin Wuttyi <wuttyi@solidplm.com>
2020-07-24 12:06:07 +0630

    44.Instantiating a Dictionary

---
+++ DictionarySnippet/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionarySnippet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Country norway = new Country("Norway", "NOR", "Europe", 5282223);
+            Country finland = new Country("Finland", "FIN", "Europe", 5511303);
+
+            var countries = new Dictionary<string, Country>();
+        }
+    }
+}
