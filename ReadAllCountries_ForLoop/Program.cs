09805a810cb9d60679cfb070ea4b1e285d94046f
Hnin Wuttyi <wuttyi@solidplm.com>
2020-07-26 11:44:00 +0630

    Add ReadAllCountries_ForLoop project 56.Enumerating - for vs. foreach

---
+++ ReadAllCountries_ForLoop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadAllCountries_ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\SolidCad\Learning\PluralSight_Courses\Beginning C# Collections\csharp-collections-beginning\Pop by Largest Final.csv";
+            CsvReader reader = new CsvReader(filePath);
+
+            List<Country> Countries = reader.ReadAllCountries();
+
+            //foreach (Country country in Countries)
+            for(int i=0; i<Countries.Count; i++)
+            {
+                Country country = Countries[i];
+                Console.WriteLine($"{country.Population}: {country.Name}");
+            }
+            Console.WriteLine($"{Countries.Count} countries");
+        }
+    }
+}
